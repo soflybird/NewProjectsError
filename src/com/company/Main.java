61c41b127315b08bc89fc18913dda36717b2755b
soflybird <74286728+soflybird@users.noreply.github.com>
2021-01-07 13:28:45 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+// package is like a folder
+package com.company;
+
+import java.util.Scanner;
+
+//public allows general access between files
+//class
+public class Main {
+//public static is method void disallows return
+//main arguments of string array and argument to pass arguments and objects
+    public static void main(String[] args) {
+        //System.out.println("Having some Java");
+        //println adds new line
+        //System.out.print("Hello Java");
+        //System.out.print("Hello Java, Whut?");
+          Scanner input = new Scanner (System.in);
+            //System.out.println("What is your name?");
+            //String firstName = input.nextLine();
+            //System.out.printf("Hello %s!\n", firstName);
+            //printf allows us to format
+            //%s looking for string and inserting firstName ! added at the end \n is for new line
+            //options for % are d decimal, t time and date, slide 21
+        //System.out.println("What is your last name?");
+        //String lastName = input.nextLine();
+        //System.out.printf("Hello, %s %s!\n", firstName, lastName);
+        //System.out.println("Enter a name: ");
+        //String name = input.nextLine();
+        //System.out.println("Give me an adverb: ");
+        //String adverb = input.nextLine();
+        //System.out.println("Give me a adjective: ");
+        //String adjective = input.nextLine();
+        //System.out.println("Give me a verb: ");
+        //String verb = input.nextLine();
+        //System.out.println("Give me a noun two: ");
+        //String nounTwo = input.nextLine();
+        //System.out.println("Give me a number: ");
+        //String number = input.nextLine();
+        //System.out.println("Give me your name: ");
+        //String yourName = input.nextLine();
+        //System.out.printf("Here is your story: \n");
+        //System.out.printf("\n Dear %s,", name);
+        //System.out.printf("\n You are %s %s and I want to be your %s!", adverb, adjective, verb);
+        //System.out.printf("\n I want to go to the %s with you in %s days.", nounTwo, number);
+
+       //double num = 5;
+        //double num2 = 10;
+
+        //boolean result = num == num2;
+
+
+
+        //System.out.println(result);
+
+        int ageLimit = 18
+
+        System.out.println("How old are you?");
+
+        int age;
+
+//This error says it expected an ; after 18
+
+
+    }
+}
